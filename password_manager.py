27ffc470c964b5bea19d045487cf0f9255a42e2b
shri_0809 <shrivratmani@gmail.com>
2024-10-12 12:09:40 +0530

    logic portion done

--- password_manager.py
+++ password_manager.py
@@ -1,41 +1,103 @@
 from cryptography.fernet import Fernet
+import sqlite3
 
-pwd = input("enter the master password: ")
+def create_connection() :
+  return sqlite3.connect('password_manager.db')
 
-key_g = Fernet.generate_key()
-with open("password-manager/secret.txt", "wb") as key_file:
-    key_file.write(key_g)
+def close_connection(connection):
+  connection.close()
 
+#//////////////////////////////////////database creation//////////////////////////////////////////////
+def create_db():
+    connection = create_connection() #database file
+    cursor = connection.cursor()
+
+    cursor.execute('''                        
+        CREATE TABLE IF NOT EXISTS keys (
+            id INTEGER PRIMARY KEY,
+            encryption_key TEXT NOT NULL
+        )
+    ''')                                          #table for key
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS passwords (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            account TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')                                          #table for passwords
+    connection.commit()
+    close_connection(connection)
+
+ 
+#//////////////////////////////////encrypted key creation and storage/////////////////////////////////////
+def initialize_key():
+  while True:
+    pwd = input("enter the master password: ")
+
+    if pwd == "shri0809":
+      key_g = Fernet.generate_key()
+      connection = create_connection()
+      cursor = connection.cursor()
+      cursor.execute('INSERT INTO keys (id,encryption_key) VALUES (?,?)', (1,key_g.decode()))
+      connection.commit()
+      close_connection(connection)
+      print("encryption of the key is done!")
+      break
+    else:
+        print("Incorrect master password!")
+        continue
+
+#////////////////////////////////////////loading of key to decrypt passwords/////////////////////////////////
 def load_key():
-  with open("password-manager/secret.txt", "rb") as key_file:
-    key = key_file.read()
-  cipher = Fernet(key)
-  return cipher
-
+  connection = create_connection()
+  cursor = connection.cursor()
+  cursor.execute('SELECT encryption_key FROM keys WHERE id = 1')
+  key = cursor.fetchone()[0].encode()
+  connection.commit()
+  close_connection(connection)
+  return Fernet(key)
+#////////////////////////////////////////deletion of key for simple re-auth////////////////////////////////////////
+def delete_key():
+   connection = create_connection()
+   cursor = connection.cursor()
+   cursor.execute("DELETE FROM keys WHERE id=1")
+   connection.commit()
+   close_connection(connection)
+#//////////////////////////////////////////username and password addition///////////////////////////////////////////////////
 def add(account,password):
   
   cipher = load_key()
-
-  with open("password-manager/password_manager.txt","a") as f:
-    f.write(f'{account}|{cipher.encrypt(password.encode()).decode()}\n')
-  
+  encrypted_password = cipher.encrypt(password.encode()).decode()
+  connection = create_connection()
+  cursor = connection.cursor()
+  cursor.execute('INSERT INTO passwords (account, password) VALUES (?, ?)', (account, encrypted_password)) 
+  connection.commit() 
+  close_connection(connection)
+#////////////////////////////////////view the username and password non decrypted format/////////////////////////////////
 def view():
-  cipher = load_key()
-  with open("password-manager/password_manager.txt","r") as f:
-    details = {}
-    for line in f:
-      user,passw = line.rstrip().split("|") 
-      
-      try:
-        passw = cipher.decrypt(passw.encode()).decode()
-        details[user] = passw
-      except Exception as e:
-        print(f"Error decrypting password for {user}: {e}")
+    cipher = load_key()
 
-      details[user] = passw
-  for key in sorted(details):
-    print(f'account: {key} || password: {details[key]}')
+    connection = create_connection()
+    cursor = connection.cursor()
+    
+ 
+    cursor.execute('SELECT account, password FROM passwords')
+    rows = cursor.fetchall()  
+    
+    close_connection(connection) 
+    for account, encrypted_password in rows:
+        try:
+            decrypted_password = cipher.decrypt(encrypted_password.encode()).decode()
+            print(f'Account: {account} || Password: {decrypted_password}')
+        except Exception as e:
+            print(f"Error decrypting password for {account}: {e}")
 
+
+#////////////////////////calling to create///////////////////////////  
+create_db()
+#//////////////////////////calling for key initialization/////////////////////////
+initialize_key()
 while True:
   mode = input("if you like to add password the write \"add\" and for viewing password write \"view\" or to quite enter \"q\": ")
 
@@ -47,6 +109,7 @@ while True:
     add(account,password)
   elif mode =="q":
     print("visit again")
+    delete_key()
     quit()
   else:
     print('invalid input')
